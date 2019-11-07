9e435296faa276972c299f7c981bde220d63a023
Ignaz Forster <iforster@suse.com>
2019-11-07 17:51:31 +0100

    Always install /etc conflict checker in dracut
    /etc/fstab.sys doesn't exist in our images, so the service wasn't running
    there. This makes it difficult to find a criteria whether the service has to
    be installed or not.
    On the other hand transactional-update-etc-cleaner is testing itself whether
    it is necessary to run, so just always install into dracut.

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 # called by dracut
-check() {
-    test -f /etc/fstab.sys || [[ -n $add_fstab  ||  -n $fstab_lines ]]
-}
-
-# called by dracut
-depends() {
-    echo fstab-sys
-}
-
-# called by dracut
 install() {
     inst_script "$moddir/transactional-update-etc-cleaner.sh" /bin/transactional-update-etc-cleaner
     inst_simple "$moddir/transactional-update-etc-cleaner.service" $systemdsystemunitdir/transactional-update-etc-cleaner.service
