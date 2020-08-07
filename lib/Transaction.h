2ae50ca7ecc3e3664990bda7e099006aad20704d
Ignaz Forster <iforster@suse.com>
2020-08-07 11:02:56 +0200

    Use SPDX identifiers instead of license headers

--- lib/Transaction.h
+++ lib/Transaction.h
@@ -1,24 +1,13 @@
 /*
+ SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-FileCopyrightText: 2020 SUSE LLC */
+
+/*
   A Transaction is unique instance, shared between all classes derived from
   the "TransactionalCommand" base class; that way it is made sure that all
   commands operate on the same snapshot. In case the destructor should be
   called before the transaction instance is closed, an eventual snapshot will
   be deleted again.
-
-  Copyright (c) 2016 - 2020 SUSE LLC
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef TRANSACTION_H_
