707a05cf36341a1b761b3e0532d4472640f1ba3a
Ignaz Forster <iforster@suse.com>
2021-01-07 18:51:35 +0100

    Use systemd event loop and add exec

--- dbus/tukitd.c
+++ dbus/tukitd.c
@@ -2,99 +2,147 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <systemd/sd-bus.h>
+#include <systemd/sd-event.h>
 
-static int method_multiply(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
-        int64_t x, y;
-        int r;
+int exec(const char* cmd, char* output) {
+    int rc;
 
-        /* Read the parameters */
-        r = sd_bus_message_read(m, "xx", &x, &y);
-        if (r < 0) {
-                fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
-                return r;
+    printf ("Executing `%s`:", cmd);
+
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) {
+        fprintf(stderr, "popen() failed!");
+        return -1;
+    }
+
+    // Does the caller want to see the output?
+    if (output != NULL) {
+        char buffer[1024];
+        while (!feof(pipe)) {
+            if (fgets(buffer, 1024, pipe) != NULL) {
+                printf("Size of output: %lu", sizeof(output));
+                char* newmem = realloc(output, sizeof(output) + 1024);
+                if (newmem == NULL) {
+                    fprintf(stderr, "realloc() failed!");
+                    free(output);
+                    return -1;
+                }
+                output = newmem;
+                strcat(output, buffer);
+            }
         }
+    }
 
-        /* Reply with the response */
-        return sd_bus_reply_method_return(m, "x", x * y);
+    rc = pclose(pipe);
+    free(output);
+    return rc;
+}
+
+static int method_open(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
+    char* command;
+    int ret;
+
+    /* Reply with the response */
+    return sd_bus_reply_method_return(m, "s", "Hallo");
 }
 
 static int method_divide(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
-        int64_t x, y;
-        int r;
+    int64_t x, y;
+    int ret;
 
-        /* Read the parameters */
-        r = sd_bus_message_read(m, "xx", &x, &y);
-        if (r < 0) {
-                fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
-                return r;
-        }
+    /* Read the parameters */
+    ret = sd_bus_message_read(m, "xx", &x, &y);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-ret));
+        return ret;
+    }
 
-        /* Return an error on division by zero */
-        if (y == 0) {
-                sd_bus_error_set_const(ret_error, "net.poettering.DivisionByZero", "Sorry, can't allow division by zero.");
-                return -EINVAL;
-        }
+    /* Return an error on division by zero */
+    if (y == 0) {
+        sd_bus_error_set_const(ret_error, "net.poettering.DivisionByZero", "Sorry, can't allow division by zero.");
+        return -EINVAL;
+    }
 
-        return sd_bus_reply_method_return(m, "x", x / y);
+    return sd_bus_reply_method_return(m, "x", x / y);
 }
 
 static const sd_bus_vtable tukit_vtable[] = {
-        SD_BUS_VTABLE_START(0),
-        SD_BUS_METHOD_WITH_ARGS("update", SD_BUS_NO_ARGS, SD_BUS_RESULT("i", ret), method_multiply, 0),
-        SD_BUS_VTABLE_END
+    SD_BUS_VTABLE_START(0),
+    SD_BUS_METHOD_WITH_ARGS("open", SD_BUS_NO_ARGS, SD_BUS_RESULT("s", ret), method_open, 0),
+    SD_BUS_VTABLE_END
 };
 
 int main(int argc, char *argv[]) {
-        sd_bus_slot *slot = NULL;
-        sd_bus *bus = NULL;
-        int r;
+    sd_bus_slot *slot = NULL;
+    sd_bus *bus = NULL;
+    sd_event *event = NULL;
+    int ret;
 
-        r = sd_bus_open_system(&bus);
-        if (r < 0) {
-                fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
-                goto finish;
-        }
+    ret = sd_bus_open_system(&bus);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-ret));
+        goto finish;
+    }
 
-        r = sd_bus_add_object_vtable(bus,
-                                     &slot, // Muss vermutlich non-floating sein
-                                     "/org/opensuse/tukit",
-                                     "org.opensuse.tukit",
-                                     tukit_vtable,
-                                     NULL);
-        if (r < 0) {
-                fprintf(stderr, "Failed to issue method call: %s\n", strerror(-r));
-                goto finish;
-        }
+    ret = sd_bus_add_object_vtable(bus,
+                                   &slot, // Muss vermutlich non-floating sein
+                                   "/org/opensuse/tukit",
+                                   "org.opensuse.tukit",
+                                   tukit_vtable,
+                                   NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to issue method call: %s\n", strerror(-ret));
+        goto finish;
+    }
 
-        /* Take a well-known service name so that clients can find us */
-        r = sd_bus_request_name(bus, "org.opensuse.tukit", 0);
-        if (r < 0) {
-                fprintf(stderr, "Failed to acquire service name: %s\n", strerror(-r));
-                goto finish;
-        }
+    /* Take a well-known service name so that clients can find us */
+    ret = sd_bus_request_name(bus, "org.opensuse.tukit", 0);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to acquire service name: %s\n", strerror(-ret));
+        goto finish;
+    }
 
-        for (;;) {
-                /* Process requests */
-                r = sd_bus_process(bus, NULL);
-                if (r < 0) {
-                        fprintf(stderr, "Failed to process bus: %s\n", strerror(-r));
-                        goto finish;
-                }
-                if (r > 0) /* we processed a request, try to process another one, right-away */
-                        continue;
+    ret = sd_event_default(&event);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to create default event loop: %s\n", strerror(-ret));
+        goto finish;
+    }
+    sigset_t ss;
+    if (sigemptyset(&ss) < 0 || sigaddset(&ss, SIGTERM) < 0 || sigaddset(&ss, SIGINT) < 0) {
+        fprintf(stderr, "Failed to set the signal set: %s\n", strerror(-ret));
+        goto finish;
+    }
+    /* Block SIGTERM first, so that the event loop can handle it */
+    if (sigprocmask(SIG_BLOCK, &ss, NULL) < 0) {
+        fprintf(stderr, "Failed to block the signals: %s\n", strerror(-ret));
+        goto finish;
+    }
+    /* Let's make use of the default handler and "floating" reference features of sd_event_add_signal() */
+    ret = sd_event_add_signal(event, NULL, SIGTERM, NULL, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Could not add signal handler for SIGTERM to event loop: %s\n", strerror(-ret));
+        goto finish;
+    }
+    ret = sd_event_add_signal(event, NULL, SIGINT, NULL, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Could not add signal handler for SIGINT to event loop: %s\n", strerror(-ret));
+        goto finish;
+    }
+    ret = sd_bus_attach_event(bus, event, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Could not add sd-bus handling to event bus: %s\n", strerror(-ret));
+        goto finish;
+    }
 
-                /* Wait for the next request to process */
-                r = sd_bus_wait(bus, (uint64_t) -1);
-                if (r < 0) {
-                        fprintf(stderr, "Failed to wait on bus: %s\n", strerror(-r));
-                        goto finish;
-                }
-        }
+    ret = sd_event_loop(event);
+    if (ret < 0) {
+        fprintf(stderr, "Error while running event loop: %s\n", strerror(-ret));
+        goto finish;
+    }
 
 finish:
-        sd_bus_slot_unref(slot);
-        sd_bus_unref(bus);
+    sd_bus_slot_unref(slot);
+    sd_bus_unref(bus);
 
-        return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+    return ret < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
-
