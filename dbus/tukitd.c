4663df88c3fb58d48fd63dacc82ebf8986b0c71f
Ignaz Forster <iforster@suse.com>
2021-04-09 09:42:03 +0200

    Use pthread for processing command
    The single threaded implementation doesn't work for when the output gets
    longer than the buffer of the pipe: The called application will just be
    blocked then until someone empties the pipe again.
    fork() is not really useful here, as it also clones the transaction object,
    so use pthreads for threading.

--- dbus/tukitd.c
+++ dbus/tukitd.c
@@ -1,12 +1,16 @@
 #include "Bindings/libtukit.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <systemd/sd-bus.h>
 #include <systemd/sd-event.h>
 #include <unistd.h>
 #include <wordexp.h>
+
+static volatile int execution_finished = 0;
+#define WORDEXP_ERROR 1000
 
 static int method_open(sd_bus_message *m, [[maybe_unused]] void *userdata, sd_bus_error *ret_error) {
     char *base;
@@ -48,25 +52,49 @@ static int method_open(sd_bus_message *m, [[maybe_unused]] void *userdata, sd_bu
     return sd_bus_reply_method_return(m, "");
 }
 
-static int method_call(sd_bus_message *m, [[maybe_unused]] void *userdata, sd_bus_error *ret_error) {
-    char *transaction;
+struct execution_args {
+    struct tukit_tx* tx;
     char *command;
-    int ret;
+};
 
-    int pipefd[2];
-    int stdout_orig = 0, stderr_orig = 0;
+static void* execution_func(void *args) {
+    int ret;
     wordexp_t p;
 
-    if (sd_bus_message_read(m, "ss", &transaction, &command) < 0) {
+    ret = wordexp(((struct execution_args*)args)->command, &p, 0);
+    if (ret != 0) {
+        if (ret == WRDE_NOSPACE) {
+            wordfree(&p);
+        }
+        ret = WORDEXP_ERROR;
+    } else {
+        ret = tukit_tx_execute(((struct execution_args*)args)->tx, p.we_wordv);
+        wordfree(&p);
+    }
+
+    execution_finished = 1;
+
+    return (void*)(intptr_t) ret;
+}
+
+static int method_call(sd_bus_message *m, [[maybe_unused]] void *userdata, sd_bus_error *ret_error) {
+    int ret;
+    char *transaction;
+    int pipefd[2];
+    int stdout_orig = 0, stderr_orig = 0;
+    struct execution_args exec_args;
+    char *output = NULL;
+
+    if (sd_bus_message_read(m, "ss", &transaction, &exec_args.command) < 0) {
         sd_bus_error_set_const(ret_error, "org.opensuse.tukit.error", "Could not read D-Bus parameters.");
         return -1;
     }
-    struct tukit_tx* tx = tukit_new_tx();
-    if (tx == NULL) {
+    exec_args.tx = tukit_new_tx();
+    if (exec_args.tx == NULL) {
         sd_bus_error_set_const(ret_error, "org.opensuse.tukit.error", tukit_get_errmsg());
         return -1;
     }
-    if (tukit_tx_resume(tx, transaction) != 0) {
+    if (tukit_tx_resume(exec_args.tx, transaction) != 0) {
         sd_bus_error_set_const(ret_error, "org.opensuse.tukit.error", tukit_get_errmsg());
         goto finish_call;
     }
@@ -95,49 +123,77 @@ static int method_call(sd_bus_message *m, [[maybe_unused]] void *userdata, sd_bu
         goto finish_call;
     }
 
-    ret = wordexp(command, &p, 0);
-    if (ret != 0) {
-        if (ret == WRDE_NOSPACE) {
-            wordfree(&p);
-        }
-        sd_bus_error_set_const(ret_error, "org.opensuse.tukit.error", "Command could not be processed.");
-        goto finish_call;
-    }
-    /*
-    if (sd_bus_reply_method_return(m, "") < 0) {
-        sd_bus_error_set_const(ret_error, "org.opensuse.tukit.error", "Error sending D-Bus reply.");
-        wordfree(&p);
-        goto finish_call;
-    }
-    */
-
     // Async from here
-    ret = tukit_tx_execute(tx, p.we_wordv);
-    wordfree(&p);
+    pthread_t execute_thread;
+    void *res;
+    ret = pthread_create(&execute_thread, NULL, execution_func, &exec_args);
+    if (ret != 0) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, ret, "Failed creating a new thread to run command.");
+        goto finish_call;
+    }
 
     char buffer[2048];
     int allocated = 2049;
     int len;
     int total = 0;
 
-    char* output = malloc(allocated);
+    output = malloc(allocated);
+    if (output == NULL) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, -1, "Failed allocating memory for output buffer.");
+        goto finish_call;
+    }
     len = fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
-    while((len = read(pipefd[0], buffer, 2048)) > 0) {
-        total = total + len;
-        if (total >= allocated) {
-            output = realloc(output, (allocated * 2) - 1);
-            allocated = (allocated * 2) - 1;
+    if (len < 0) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, len, "Failed setting non-blocking pipe.");
+        goto finish_call;
+    }
+    while (execution_finished < 2) {
+        if (execution_finished == 1)
+            execution_finished = 2;
+        while((len = read(pipefd[0], buffer, 2048)) > 0) {
+            total = total + len;
+            if (total >= allocated) {
+                output = realloc(output, (allocated * 2) - 1);
+                if (output == NULL) {
+                    ret = -1;
+                }
+                allocated = (allocated * 2) - 1;
+            }
+            strncpy(&output[total - len], buffer, len);
         }
-        strncpy(&output[total - len], buffer, len);
+        if (len < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+            sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, len, "Error reading pipe.");
+            goto finish_call;
+        }
+        usleep(500000);
     }
     output[total] = '\0';
+    // Don't abort while reading the pipe already, it may block if full otherwise
+    if (ret != 0) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, -1, "Failed re-allocating memory for output buffer.");
+        goto finish_call;
+    }
 
-    ret = tukit_tx_keep(tx);
+    pthread_join(execute_thread, &res);
+    ret = (intptr_t) res;
+    if (ret == WORDEXP_ERROR) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, -1, "Error parsing command.");
+        goto finish_call;
+    }
+
+    if (tukit_tx_keep(exec_args.tx) != 0) {
+        sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandError", "sis", transaction, -1, tukit_get_errmsg());
+        goto finish_call;
+    }
 
     sd_bus_emit_signal(sd_bus_message_get_bus(m), "/org/opensuse/tukit", "org.opensuse.tukit", "CommandExecuted", "sis", transaction, ret, output);
-    free(output);
 
 finish_call:
+    if (output != NULL)
+        free(output);
+
+    pthread_join(execute_thread, NULL);
+
     if (stdout_orig != 0) {
         dup2(stdout_orig, 1);
         close(stdout_orig);
@@ -147,10 +203,11 @@ finish_call:
         close(stderr_orig);
     }
 
-    tukit_free_tx(tx);
+    tukit_free_tx(exec_args.tx);
     if (ret) {
         //return ret;
     }
+    execution_finished = 0;
     return sd_bus_reply_method_return(m, "");
 }
 
@@ -160,6 +217,7 @@ static const sd_bus_vtable tukit_vtable[] = {
     SD_BUS_METHOD_WITH_ARGS("call", SD_BUS_ARGS("s", transaction, "s", command), SD_BUS_NO_RESULT, method_call, 0),
     SD_BUS_SIGNAL_WITH_ARGS("TransactionOpened", SD_BUS_ARGS("s", snapshot), 0),
     SD_BUS_SIGNAL_WITH_ARGS("CommandExecuted", SD_BUS_ARGS("s", snapshot, "i", returncode, "s", output), 0),
+    SD_BUS_SIGNAL_WITH_ARGS("CommandError", SD_BUS_ARGS("s", snapshot, "i", returncode, "s", message), 0),
     SD_BUS_VTABLE_END
 };
 
