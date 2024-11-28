a024087ae15e26cccc9341e6c6cf12363e8bbec5
Ignaz Forster <iforster@suse.com>
2024-11-28 14:31:28 +0100

    Dracut: Use new sync-etc-state

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -2,9 +2,9 @@
 
 # called by dracut
 install() {
-    inst_script "$moddir/transactional-update-etc-cleaner.sh" /bin/transactional-update-etc-cleaner
-    inst_simple "$moddir/transactional-update-etc-cleaner.service" $systemdsystemunitdir/transactional-update-etc-cleaner.service
+    inst_script "/usr/libexec/transactional-update-sync-etc-state" /bin/transactional-update-sync-etc-state
+    inst_simple "$moddir/transactional-update-sync-etc-state.service" $systemdsystemunitdir/transactional-update-sync-etc-state.service
+    inst_simple "$moddir/transactional-update-sync-etc-state.sh" /bin/transactional-update-sync-etc-state.sh
     mkdir -p "${initdir}/$systemdsystemunitdir/initrd.target.wants"
-    ln_r "$systemdsystemunitdir/transactional-update-etc-cleaner.service" "$systemdsystemunitdir/initrd.target.wants/transactional-update-etc-cleaner.service"
-    inst_multiple stat rmdir
+    ln_r "$systemdsystemunitdir/transactional-update-sync-etc-state.service" "$systemdsystemunitdir/initrd.target.wants/transactional-update-sync-etc-state.service"
 }
---
+++ dracut/transactional-update-sync-etc-state.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+mount --target-prefix /sysroot --fstab /sysroot/etc/fstab /.snapshots
+mount --bind /sysroot/etc /sysroot/etc
+mount -o remount,rw /sysroot/etc
+parentid="$(cat /sysroot/etc/etc.syncpoint/transactional-update.comparewith)"
+
+# Migration from overlayfs based system
+if [ -e "/sysroot/var/lib/overlay/${parentid}" ]; then
+  cp "/sysroot/var/lib/overlay/${parentid}"/etc/fstab /tmp/tu-fstab
+  sed -i "s#:/sysroot/etc,#:/sysroot/.snapshots/${parentid}/snapshot/etc,#g" /tmp/tu-fstab
+  mount --target-prefix "/sysroot/.snapshots/${parentid}/snapshot" --fstab /tmp/tu-fstab /etc
+fi
+
+/bin/transactional-update-sync-etc-state "/sysroot/.snapshots/${parentid}/snapshot/etc" "/sysroot/etc" "/sysroot/etc/etc.syncpoint"
